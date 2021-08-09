8250081eadf786cb1ac66797d018b74250bd27e3
lizhixin <lzx920911@gmail.com>
2021-08-09 09:26:52 +0800

    Initial commit

---
+++ src/main/java/com/lzx/peachutil/PeachUtilApplication.java
@@ -0,0 +1,13 @@
+package com.lzx.peachutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeachUtilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeachUtilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lzx/peachutil/PeachUtilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzx.peachutil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeachUtilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
