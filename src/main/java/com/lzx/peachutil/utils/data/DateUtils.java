25eafe3b106831715e571f68b724169f312a82c1
lizhixin <lzx920911@gmail.com>
2021-08-09 15:19:25 +0800

    DateUtils添加新方法；LocalDateTimeUtils类初始化

--- src/main/java/com/lzx/peachutil/utils/data/DateUtils.java
+++ src/main/java/com/lzx/peachutil/utils/data/DateUtils.java
@@ -1,19 +1,24 @@
 package com.lzx.peachutil.utils.data;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.time.DateFormatUtils;
 
 import java.lang.management.ManagementFactory;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+
 /**
  * DATE 时间工具类
  *
- * @author     lizhixin
- * @date       2021/8/6 15:49
+ * @author lizhixin
+ * @date 2021/8/6 15:49
  */
-public class DateUtils extends org.apache.commons.lang3.time.DateUtils
-{
+@Slf4j
+public class DateUtils extends org.apache.commons.lang3.time.DateUtils {
     public static String YYYY = "yyyy";
 
     public static String YYYY_MM = "yyyy-MM";
@@ -23,120 +28,149 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils
     public static String YYYYMMDDHHMMSS = "yyyyMMddHHmmss";
 
     public static String YYYY_MM_DD_HH_MM_SS = "yyyy-MM-dd HH:mm:ss";
-    
+
     private static final String[] parsePatterns = {
-            "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM", 
+            "yyyy-MM-dd", "yyyy-MM-dd HH:mm:ss", "yyyy-MM-dd HH:mm", "yyyy-MM",
             "yyyy/MM/dd", "yyyy/MM/dd HH:mm:ss", "yyyy/MM/dd HH:mm", "yyyy/MM",
             "yyyy.MM.dd", "yyyy.MM.dd HH:mm:ss", "yyyy.MM.dd HH:mm", "yyyy.MM"};
 
     /**
-     * 获取当前Date型日期
-     * 
-     * @return Date() 当前日期
+     * 获取当前日期
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:18
      */
-    public static Date getNowDate()
-    {
+    public static Date getNowDate() {
         return new Date();
     }
 
     /**
      * 获取当前日期, 默认格式为yyyy-MM-dd
-     * 
-     * @return String
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:19
      */
-    public static String getDate()
-    {
+    public static String getDate() {
         return dateTimeNow(YYYY_MM_DD);
     }
 
-    public static final String getTime()
-    {
+    /**
+     * 获取当前日期, 默认格式为yyyy-MM-dd HH:mm:ss
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:19
+     */
+    public static final String getTime() {
         return dateTimeNow(YYYY_MM_DD_HH_MM_SS);
     }
 
-    public static final String dateTimeNow()
-    {
+    /**
+     * 获取当前日期, 默认格式为yyyyMMddHHmmss
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:19
+     */
+    public static final String dateTimeNow() {
         return dateTimeNow(YYYYMMDDHHMMSS);
     }
 
-    public static final String dateTimeNow(final String format)
-    {
+    /**
+     * 获取当前日期, 根据传入的格式进行格式化
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:19
+     */
+    public static final String dateTimeNow(final String format) {
         return parseDateToStr(format, new Date());
     }
 
-    public static final String dateTime(final Date date)
-    {
+    /**
+     * 根据传入的时间格式化, 默认格式为yyyy-MM-dd
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:21
+     */
+    public static final String dateTime(final Date date) {
         return parseDateToStr(YYYY_MM_DD, date);
     }
 
-    public static final String parseDateToStr(final String format, final Date date)
-    {
-        return new SimpleDateFormat(format).format(date);
-    }
-
-    public static final Date dateTime(final String format, final String ts)
-    {
-        try
-        {
-            return new SimpleDateFormat(format).parse(ts);
-        }
-        catch (ParseException e)
-        {
-            throw new RuntimeException(e);
-        }
+    /**
+     * 根据传入的时间格式化, 默认格式为yyyy-MM-dd HH:mm:ss
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:21
+     */
+    public static final String dateTimeFormatYMDHMS(final Date date) {
+        return parseDateToStr(YYYY_MM_DD_HH_MM_SS, date);
     }
 
     /**
-     * 日期路径 即年/月/日 如2018/08/08
+     * 根据传入的时间与格式，格式化数据
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:22
      */
-    public static final String datePath()
-    {
+    public static final String parseDateToStr(final String format, final Date date) {
+        return new SimpleDateFormat(format).format(date);
+    }
+
+    /**
+     * 日期路径 即年/月/日 如2021/08/08
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:24
+     */
+    public static final String datePath() {
         Date now = new Date();
         return DateFormatUtils.format(now, "yyyy/MM/dd");
     }
 
     /**
-     * 日期路径 即年/月/日 如20180808
+     * 日期路径 即年/月/日 如20210808
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:24
      */
-    public static final String dateTime()
-    {
+    public static final String dateTime() {
         Date now = new Date();
         return DateFormatUtils.format(now, "yyyyMMdd");
     }
 
     /**
      * 日期型字符串转化为日期 格式
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:24
      */
-    public static Date parseDate(Object str)
-    {
-        if (str == null)
-        {
+    public static Date parseDate(Object str) {
+        if (str == null) {
             return null;
         }
-        try
-        {
+        try {
             return parseDate(str.toString(), parsePatterns);
-        }
-        catch (ParseException e)
-        {
+        } catch (ParseException e) {
             return null;
         }
     }
-    
+
     /**
      * 获取服务器启动时间
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:25
      */
-    public static Date getServerStartDate()
-    {
+    public static Date getServerStartDate() {
         long time = ManagementFactory.getRuntimeMXBean().getStartTime();
         return new Date(time);
     }
 
     /**
      * 计算两个时间差
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:25
      */
-    public static String getDatePoor(Date endDate, Date nowDate)
-    {
+    public static String getDatePoor(Date endDate, Date nowDate) {
         long nd = 1000 * 24 * 60 * 60;
         long nh = 1000 * 60 * 60;
         long nm = 1000 * 60;
@@ -156,8 +190,281 @@ public class DateUtils extends org.apache.commons.lang3.time.DateUtils
 
     /**
      * 获取当前时间戳
+     *
+     * @author lizhixin
+     * @date 2021/8/9 11:25
      */
-    public static Long getLongTimeMillis(){
+    public static Long getLongTimeMillis() {
         return System.currentTimeMillis();
     }
+
+
+    /**
+     * 获取一天的开始时间，2021-08-08 00:00:00
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDayStart(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), gregorianCalendar.get(Calendar.DATE),
+                0, 0, 0);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取一天的结束时间，2021-08-08 23:59:59
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDayEnd(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), gregorianCalendar.get(Calendar.DATE),
+                23, 59, 59);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取当天的开始时间，2021-08-08 00:00:00
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDayStart() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), gregorianCalendar.get(Calendar.DATE),
+                0, 0, 0);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取当天的结束时间，2021-08-08 23:59:59
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDayEnd() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), gregorianCalendar.get(Calendar.DATE),
+                23, 59, 59);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取对应时间月份的起始日期，2021-08-01 00:00:00
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getMonthStart(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), 1,
+                0, 0, 0);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取对应时间月份的最后日期，2021-08-01 23:59:59
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getMonthEnd(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        int maximum = gregorianCalendar.getMaximum(Calendar.DAY_OF_MONTH);
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), maximum,
+                23, 59, 59);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取当月月份的起始日期，2021-08-01 00:00:00
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getMonthStart() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), 1,
+                0, 0, 0);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取当月月份的最后日期，2021-08-01 23:59:59
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getMonthEnd() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        int maximum = gregorianCalendar.getMaximum(Calendar.DAY_OF_MONTH);
+        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR), gregorianCalendar.get(Calendar.MONTH), maximum,
+                23, 59, 59);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 以指定时间为基准，向后num天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDateAdd(Date date, int num) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        gregorianCalendar.add(Calendar.DATE, num);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 以指定时间为基准，向前num天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDateDel(Date date, int num) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        gregorianCalendar.add(Calendar.DATE, -num);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 以当前时间为基准，向后num天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDateAdd(int num) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        gregorianCalendar.add(Calendar.DATE, num);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 以当前时间为基准，向前num天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Date getDateDel(int num) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        gregorianCalendar.add(Calendar.DATE, -num);
+        return gregorianCalendar.getTime();
+    }
+
+    /**
+     * 获取当天在一周中的位置
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Integer getDayOfWeek() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        return gregorianCalendar.get(Calendar.DAY_OF_WEEK);
+    }
+
+    /**
+     * 获取对应时间在一周中的位置
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Integer getDayOfWeek(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        return gregorianCalendar.get(Calendar.DAY_OF_WEEK);
+    }
+
+    /**
+     * 获取当天是当月中的第多少天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Integer getDateOfMonth() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        return gregorianCalendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * 获取对应时间是当月中的第多少天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Integer getDateOfMonth(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        return gregorianCalendar.get(Calendar.DAY_OF_MONTH);
+    }
+
+    /**
+     * 获取当天是一年中的第多少天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Integer getDateOfYear() {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(getNowDate());
+        return gregorianCalendar.get(Calendar.DAY_OF_YEAR);
+    }
+
+    /**
+     * 获取对应时间是一年中的第多少天
+     *
+     * @author lizhixin
+     * @date 2021/8/9 14:35
+     */
+    public static Integer getDateOfYear(Date date) {
+        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+        gregorianCalendar.setTime(date);
+        return gregorianCalendar.get(Calendar.DAY_OF_YEAR);
+    }
+
+
+
+
+    public static void main(String[] args) {
+//        GregorianCalendar gregorianCalendar = new GregorianCalendar();
+//
+//        log.info(String.valueOf(gregorianCalendar.getTimeZone()));
+//        gregorianCalendar.setTime(new Date());
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.YEAR)));
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.MONTH)));
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.DATE)));
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.DAY_OF_WEEK)));
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.HOUR_OF_DAY)));
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.MINUTE)));
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.SECOND)));
+//        log.info(String.valueOf(gregorianCalendar.get(Calendar.MILLISECOND)));
+//
+//        gregorianCalendar.set(gregorianCalendar.get(Calendar.YEAR),gregorianCalendar.get(Calendar.MONTH),gregorianCalendar.get(Calendar.DATE),
+//                23,59,59);
+//        log.info(parseDateToStr(YYYY_MM_DD_HH_MM_SS,gregorianCalendar.getTime()));
+
+//        log.info(getDayOfWeek(getNowDate()).toString());
+
+//        log.info(dateTimeFormatYMDHMS(getMonthStart(getNowDate())));
+//        log.info(dateTimeFormatYMDHMS(getMonthEnd(getNowDate())));
+
+//        log.info(dateTimeFormatYMDHMS(getDateAdd(23)));
+//        log.info(dateTimeFormatYMDHMS(getDateDel(10)));
+    }
+
 }
---
+++ src/main/java/com/lzx/peachutil/utils/localDatetime/LocalDateTimeUtils.java
@@ -0,0 +1,201 @@
+package com.lzx.peachutil.utils.localDatetime;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.Date;
+
+/**
+ * LocalDateTime is used to Java8中的时间类
+ * <p>
+ * 为什么需要 LocalDate、LocalTime、LocalDateTime:
+ * 1. Date如果不格式化，打印出的日期可读性差,使用 SimpleDateFormat对时间进行格式化，但 SimpleDateFormat是线程不安全的
+ * 2. calendar是共享变量，并且这个共享变量没有做线程安全控制。
+ * 当多个线程同时使用相同的 SimpleDateFormat对象【如用 static修饰的 SimpleDateFormat】调用format方法时，
+ * 多个线程会同时调用calendar.setTime方法，可能一个线程刚设置好 time值另外的一个线程马上把设置的 time值给修改了,
+ * 导致返回的格式化时间可能是错误的.
+ * 3. Date对时间处理比较麻烦，比如想获取某年、某月、某星期，以及 n天以后的时间
+ * <p>
+ * <p>
+ * Use Example:
+ * <p>
+ * LocalDateTimeUtils.formatNow("yyyy年MM月dd日 HH:mm:ss")
+ * "年:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.YEARS)
+ * "月:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.MONTHS)
+ * "日:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.DAYS)
+ * "半日:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.HALF_DAYS)
+ * "小时:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.HOURS)
+ * "分钟:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.MINUTES)
+ * "秒:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.SECONDS)
+ * "毫秒:" + LocalDateTimeUtils.betweenTwoTime(start, end, ChronoUnit.MILLIS)
+ * --------------------------------------------------------------------------------------------
+ * 增加二十分钟
+ * LocalDateTimeUtils.formatTime(LocalDateTimeUtils.plus(LocalDateTime.now(),20,ChronoUnit.MINUTES), "yyyy年MM月dd日 HH:mm")
+ * 增加两年
+ * LocalDateTimeUtils.formatTime(LocalDateTimeUtils.plus(LocalDateTime.now(),2, ChronoUnit.YEARS), "yyyy年MM月dd日 HH:mm")
+ */
+
+/**
+ * LocalDateTime 时间工具类
+ *
+ * @author lizhixin
+ * @date 2021/8/9 11:28
+ */
+public class LocalDateTimeUtils {
+
+    public static String FORMAT_Y_M_D = "yyyy-MM-dd";
+
+    /**
+     * Date转换为 LocalDateTime
+     *
+     * @param date date
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime convertDateToLocalDateTime(Date date) {
+        return LocalDateTime.ofInstant(date.toInstant(), ZoneId.systemDefault());
+    }
+
+
+    /**
+     * LocalDateTime转换为 Date
+     *
+     * @param time time
+     * @return {@link Date}
+     */
+    public static Date convertLocalDateTimeToDate(LocalDateTime time) {
+        return Date.from(time.atZone(ZoneId.systemDefault()).toInstant());
+    }
+
+
+    /**
+     * 获取指定日期的毫秒
+     *
+     * @param time time
+     * @return {@link Long}
+     */
+    public static Long getMilliByTime(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().toEpochMilli();
+    }
+
+
+    /**
+     * 获取指定日期的秒
+     *
+     * @param time time
+     * @return {@link Long}
+     */
+    public static Long getSecondsByTime(LocalDateTime time) {
+        return time.atZone(ZoneId.systemDefault()).toInstant().getEpochSecond();
+    }
+
+
+    /**
+     * 获取指定时间的指定格式
+     *
+     * @param time    time
+     * @param pattern pattern
+     * @return {@link String}
+     */
+    public static String formatTime(LocalDateTime time, String pattern) {
+        return time.format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+    /**
+     * 获取当前时间的指定格式
+     *
+     * @param pattern pattern
+     * @return {@link String}
+     */
+    public static String formatNow(String pattern) {
+        return formatTime(LocalDateTime.now(), pattern);
+    }
+
+
+    /**
+     * 日期加上一个数,根据 field不同加不同值,field为 ChronoUnit.*
+     *
+     * @param time   time
+     * @param number number
+     * @param field  field
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime plus(LocalDateTime time, long number, TemporalUnit field) {
+        return time.plus(number, field);
+    }
+
+
+    /**
+     * 日期减去一个数,根据 field不同减不同值,field参数为 ChronoUnit.*
+     *
+     * @param time   time
+     * @param number number
+     * @param field  field
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime minu(LocalDateTime time, long number, TemporalUnit field) {
+        return time.minus(number, field);
+    }
+
+
+    /**
+     * 获取两个日期的差  field参数为 ChronoUnit.*
+     *
+     * @param startTime startTime
+     * @param endTime   endTime
+     * @param field     field
+     * @return {@link long}
+     */
+    public static long betweenTwoTime(LocalDateTime startTime, LocalDateTime endTime, ChronoUnit field) {
+        Period period = Period.between(LocalDate.from(startTime), LocalDate.from(endTime));
+        if (field == ChronoUnit.YEARS) {
+            return period.getYears();
+        }
+        if (field == ChronoUnit.MONTHS) {
+            return period.getYears() * 12 + period.getMonths();
+        }
+        return field.between(startTime, endTime);
+    }
+
+
+    /**
+     * 获取一天的开始时间，2020,5,11 00:00
+     *
+     * @param time time
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime getDayStart(LocalDateTime time) {
+        return time.withHour(0)
+                .withMinute(0)
+                .withSecond(0)
+                .withNano(0);
+    }
+
+
+    /**
+     * 获取一天的结束时间，2020,5,11 23:59:59.999999999
+     *
+     * @param time time
+     * @return {@link LocalDateTime}
+     */
+    public static LocalDateTime getDayEnd(LocalDateTime time) {
+        return time.withHour(23)
+                .withMinute(59)
+                .withSecond(59)
+                .withNano(999999999);
+    }
+
+    /**
+     * 获取当前时间
+     *
+     * @author lizhixin
+     * @date 2021/7/15 14:25
+     */
+    public static LocalDateTime getNewDay() {
+        return LocalDateTime.now(ZoneId.systemDefault());
+    }
+}
